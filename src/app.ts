9e8a4e7417787e6caf011391652021ba56c3298a
altinat <48298065+altinat@users.noreply.github.com>
2021-08-01 09:09:01 +0700

    Fixed things up

--- src/app.ts
+++ src/app.ts
@@ -4,55 +4,66 @@ import { bbJsonToSrt } from "./utils/index.ts";
 const cli = cac("bilibili");
 
 cli
-  .command("info <sId>", "View info")
-  .action(async (sId, options) => {
-    const res = await fetch(
-      `https://api.global.bilibili.com/intl/gateway/v2/ogv/view/app/season?&season_id=${sId}`,
-    );
-    // deno-lint-ignore camelcase
-    const { result: { season_id, title, evaluate, modules } } = await res
-      .json();
-
-    console.log(`[${season_id}] ${title}`);
-    console.log(`[Description]\r\n${evaluate}`);
-    console.log("[Eposodes]");
-    // deno-lint-ignore no-explicit-any
-    modules[0].data.episodes.map((item: { title: any; id: any }) => {
-      console.log(`(${item.title}) - [${item.id}]`);
-    });
+  .command("epinfo <epId>", "View EP info")
+  .action(async (epId, options) => {
+    await fetch(
+      `https://api.global.bilibili.com/intl/gateway/web/view?oid=${epId}&tp=3&s_locale=th_TH`,
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.error(err);
+      });
   });
 
 cli
-  .command("get <epId>", "Get subtitle")
+  .command("sub <epId>", "Get subtitle")
   .action(async (epId, options) => {
-    const res = await fetch(
-      `https://api.global.bilibili.com/intl/gateway/m/subtitle?ep_id=${epId}`,
-    );
-    const { data: { subtitles } } = await res.json();
+    await fetch(
+      `https://api.biliintl.com/intl/gateway/m/subtitle?build=1&ep_id=${epId}&s_locale=th_TH`,
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.error(err);
+      });
 
     // deno-lint-ignore no-explicit-any
-    subtitles.map(async (item: any) => {
-      const { key, url } = item;
-      const dest = `[${key}] ${path.basename(url).replace(".json", ".srt")}`;
-      const res = await fetch(url);
-      const data = await res.json();
-      bbJsonToSrt(dest, data);
-      console.log(dest);
-    });
+    //subtitles.map(async (item: any) => {
+    // const { key, url } = item;
+    // const dest = `[${key}] ${path.basename(url).replace(".json", ".srt")}`;
+    //  const res = await fetch(url);
+    //  const data = await res.json();
+    // bbJsonToSrt(dest, data);
+    //  console.log(dest);
+    //});
+  });
+
+cli
+  .command("vid <epId>", "Video Download (Soon)")
+  .action(async (epId, options) => {
+    await fetch(
+      `https://api.biliintl.com/intl/gateway/web/playurl?&device=wap&ep_id=${epId}&platform=web&qn=64&s_locale=th_TH`,
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.error(err);
+      });
   });
 
 cli
   .command("list", "Lists of anime")
   .action(async () => {
     const res = await fetch(
-      "https://api.global.bilibili.com/intl/gateway/v2/ogv/season/index/result?&build=1000210&c_locale=&mobi_app=bstar_a&order=0&page=1&pagesize=1&platform=android&s_locale=en_TH",
+      "https://api.biliintl.com/intl/gateway/v2/ogv/season/index/result?&build=1000210&c_locale=&mobi_app=bstar_a&order=0&page=1&pagesize=1&platform=web&s_locale=th_TH",
     );
     const { data: { total } } = await res.json();
     const round = Math.ceil(total / 100);
 
     for (let page = 0; page < round; page++) {
       const res = await fetch(
-        `https://api.global.bilibili.com/intl/gateway/v2/ogv/season/index/result?&build=1000210&c_locale=&mobi_app=bstar_a&order=0&page=${page}&pagesize=100&platform=android&s_locale=en_TH`,
+        `https://api.biliintl.com/intl/gateway/v2/ogv/season/index/result?&build=1000210&c_locale=&mobi_app=bstar_a&order=0&page=${page}&pagesize=100&platform=web&s_locale=th_TH`,
       );
       const { data: { list } } = await res.json();
 
@@ -62,6 +73,6 @@ cli
   });
 cli.help();
 
-cli.version("0.0.2");
+cli.version("0.0.1");
 
 cli.parse();
